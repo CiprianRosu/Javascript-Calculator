43420b38638ee82a25fc4a16e3252951db1d8791
Ciprian Andrei Rosu <cip.rosu@yahoo.com>
2022-11-06 15:05:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ buttons.map(button =>{
         case 'C':
             display.innerText='';
             break;
+        case '<-':
+            if(display.innerText){
+                display.innerText = display.innerText.slice(0 , -1);
+            }
+            break;
+        case '=':
+            try{
+                display.innerText = eval(display.innerText);
+            }catch{
+                display.innerText='Error';
+            }
+            break;        
         default:
             display.innerHTML +=  e.target.innerText;
      }
